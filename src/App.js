cb0a5dac702de388e182ea8149006c897a136ac1
Oganesyan Artur <arturris@mail.ru>
2018-06-18 10:54:33 -0500

    Basic Setup in Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Staff from './components/pages/Staff';
+import  Home from './components/pages/Home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <h1>Hello!</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <div className="container">
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/staff" exact={true} component={Staff} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
